d5af372a1ae73cbb1cbd9f75d4a44c58712c0f28
Jonathan Birge <birge@mit.edu>
2022-03-05 14:54:21 -0500

    ios framework template

---
+++ OptimizationKit/OptimizationKit.docc/OptimizationKit.md
@@ -0,0 +1,8 @@
+# ``OptimizationKit``
+
+Framework providing object-oriented optimization and non-linear regression interfaces.
+
+## Overview
+
+By implementing the ``Fittable`` protocol, you can make any object model support non-linear regression.
+
---
+++ OptimizationKit/OptimizationKit.h
@@ -0,0 +1,18 @@
+//
+//  OptimizationKit.h
+//  OptimizationKit
+//
+//  Created by Jonathan Birge on 3/5/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OptimizationKit.
+FOUNDATION_EXPORT double OptimizationKitVersionNumber;
+
+//! Project version string for OptimizationKit.
+FOUNDATION_EXPORT const unsigned char OptimizationKitVersionString[];
+
+// Import all the public headers of your framework using statements like #import <OptimizationKit/PublicHeader.h>
+
+
